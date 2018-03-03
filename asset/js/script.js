71a0cd547b63328dd93373fa09a8332ce3151f40
David Traparic <david.traparic@etu.univ-poitiers.fr>
2018-03-03 17:28:45 +0100

    tests js fonctionnels puis emmenent bien sur la page php

--- asset/js/script.js
+++ asset/js/script.js
@@ -6,8 +6,10 @@ $(function(){
     // $msginput = $('.input__msg');
 
   function isMailValid(){
-    let patternMail = new RegExp("^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$");
+    let patternMail = new RegExp("^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$");
     let mail = $mailinput.val();
+    console.log(mail);
+    console.log(patternMail.test(mail));
 
     return patternMail.test(mail);
   }
@@ -16,14 +18,17 @@ $(function(){
     let patternTel = new RegExp("^0+[0-9]{9}$");
     let tel = $telinput.val();
 
+    console.log(tel);
+    console.log(patternTel.test(tel));
     return patternTel.test(tel);
   }
 
   $('button').click(function(){
+    console.log("ok");
     if(isMailValid() && isTelValid()){
       $('form').submit();
     }
   });
 
 
-})(jQuery);
+});
